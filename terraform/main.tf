cc13060c3ad455a72ab3d2424e3b06725e9732db
harshithakashi <harshithakg13@gmail.com>
2025-12-27 22:49:43 -0600

    Add Terraform main skeleton

---
+++ terraform/main.tf
@@ -0,0 +1,13 @@
+/*
+DeFtunes Lakehouse Capstone - Terraform (Skeleton)
+
+This folder represents Infrastructure-as-Code for:
+- S3 Data Lake (landing/transform buckets or prefixes)
+- AWS Glue Jobs + IAM roles
+- Glue Data Catalog databases/tables (Iceberg)
+- Redshift cluster + Spectrum external schema
+- Supporting networking/permissions
+
+Note: In the academic lab environment, values are provided by CloudFormation.
+In a real setup, variables and modules would be used for reusable deployments.
+*/
